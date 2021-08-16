0ec4edbb298beb5cec05c409d899a0e10e481498
shivam1234-hub <76586960+shivam1234-hub@users.noreply.github.com>
2021-08-16 16:21:05 +0530

    first commit

---
+++ mapdata.js
@@ -0,0 +1,222 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "450", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "black",
+    state_hover_color: "#3B729F",
+    state_url: "https://simplemaps.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "http://127.0.0.1:5501/test1.html",
+    location_size: 25,
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "More-Info",
+    popups: "on_click",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      description: "custom d-1"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      description: "custom d-2"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      description: "custom d-3"
+    },
+    "4": {
+      name: "Assam",
+      description: "custom d-4"
+    },
+    "5": {
+      name: "Bihar",
+      description: "custom d-5"
+    },
+    "6": {
+      name: "Chandigarh",
+      description: "custom d-6"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      description: "custom d-7"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      description: "custom d-8"
+    },
+    "9": {
+      name: "Daman And Diu",
+      description: "custom d-9"
+    },
+    "10": {
+      name: "Delhi",
+      description: "custom d-10"
+    },
+    "11": {
+      name: "Goa"
+    },
+    "12": {
+      name: "Gujarat"
+    },
+    "13": {
+      name: "Haryana"
+    },
+    "14": {
+      name: "Himachal Pradesh"
+    },
+    "16": {
+      name: "Jharkhand"
+    },
+    "17": {
+      name: "Karnataka"
+    },
+    "18": {
+      name: "Kerala"
+    },
+    "19": {
+      name: "Lakshadweep"
+    },
+    "20": {
+      name: "Madhya Pradesh"
+    },
+    "21": {
+      name: "Maharashtra"
+    },
+    "22": {
+      name: "Manipur"
+    },
+    "23": {
+      name: "Meghalaya"
+    },
+    "24": {
+      name: "Mizoram"
+    },
+    "25": {
+      name: "Nagaland"
+    },
+    "26": {
+      name: "Orissa"
+    },
+    "27": {
+      name: "Puducherry"
+    },
+    "28": {
+      name: "Punjab"
+    },
+    "29": {
+      name: "Rajasthan"
+    },
+    "30": {
+      name: "Sikkim"
+    },
+    "31": {
+      name: "Tamil Nadu"
+    },
+    "32": {
+      name: "Tripura"
+    },
+    "33": {
+      name: "Uttar Pradesh"
+    },
+    "34": {
+      name: "Uttaranchal"
+    },
+    "35": {
+      name: "West Bengal"
+    },
+    "36": {
+      name: "Jammu And Kashmir"
+    },
+    "37": {
+      name: "Telangana"
+    },
+    "38": {
+      name: "Ladakh"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 18.987807,
+      lng: 72.836447,
+      name: "Mumbai",
+      description: "Ead-Mumbai"
+    },
+    "1": {
+      lat: "27.0238",
+      lng: "74.2179",
+      name: "Rajasthan",
+      description: "Ead-Rajasthan"
+    },
+    "2": {
+      lat: "22.5726",
+      lng: "88.3639",
+      name: "Kolkata",
+      description: "Ead-Kolkata"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
